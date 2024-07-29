1044613db6689ec36142a8b048b433f5d9685f3d
b3liott <eliott.georges8@gmail.com>
2024-07-29 18:47:28 +0200

    ok

--- Sn00pXss/modules/vector_detector/detector.py
+++ Sn00pXss/modules/vector_detector/detector.py
@@ -3,7 +3,5 @@ from modules.requestor.requestor import Requestor
 
 
 def detect(requestor: Requestor, requestModel: RequestModel):
-    # 1- detect if our inout goes in the <script> tag
-    # 2- detect if the input is reflected in the page
     pass
 
