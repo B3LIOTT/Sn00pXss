ccaa69513b799fa25833f6fe7fd18b9f752c2454
B3LIOTT <55959873+B3LIOTT@users.noreply.github.com>
2024-09-13 21:36:42 +0200

    Update detector.py

--- Sn00pXss/modules/vector_detector/detector.py
+++ Sn00pXss/modules/vector_detector/detector.py
@@ -1,8 +1,13 @@
 from models import RequestModel
 from modules.requestor.requestor import Requestor
+import re
 
 
 def detect_attack_type(requestor: Requestor, requestModel: RequestModel):
-    # TODO
-    pass
-
+    # TODO: detecter les potentiels vecteurs d'attaque
+    # ESCAPE_JS: détecter si l'input se trouve dans un script JS
+    # ESCAPE_HTML: détecter si l'input se trouve dans du HTML, dans une balise sous forme de texte
+    # ESCAPE_ATTR: détecter si l'input se trouve dans un attribut HTML, un event
+    
+    raise NotImplementedError("Not implemented yet")
+    
